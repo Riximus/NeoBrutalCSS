7af9a1cecc46d99b033a1e04ea604cb74661ffa5
Riximus <riximuslol@gmail.com>
2024-01-25 19:03:39 +0100

    🚧 added a select prototype

--- src/stories/elements/selects/Select.js
+++ src/stories/elements/selects/Select.js
@@ -1,5 +1,22 @@
 export const createSelect = () => {
 	const select = document.createElement('select');
+
+	// Placeholder as the first option
+	const placeholderOption = document.createElement('option');
+	placeholderOption.value = '';
+	placeholderOption.text = 'Please select an option';
+	placeholderOption.disabled = true;
+	placeholderOption.selected = true;
+	select.appendChild(placeholderOption);
+
+	// Loop to add options
+	for (let i = 1; i <= 5; i++) {
+		const optionElement = document.createElement('option');
+		optionElement.value = `option${i}`;
+		optionElement.text = `Option ${i}`;
+		select.appendChild(optionElement);
+	}
+
 	select.className = 'neo-select';
 	return select;
 };
